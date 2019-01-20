51468decfeca3953738248b12bde2d02d0d0ec98
CarlosCL98 <carmer.cl98@gmail.com>
2019-01-20 02:10:54 -0500

    Solucionado conflicto con Anagrams.java

--- Anagrams/src/Anagrams.java
+++ Anagrams/src/Anagrams.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Anagrams {
-	
+
 	public static HashMap<Character, Integer> countWords(ArrayList<Character> word1) {
 		HashMap<Character, Integer> words1= new HashMap<>();
 		for (int i = 0; i<word1.size(); i++) {
@@ -14,7 +14,7 @@ public class Anagrams {
 		}
 		return words1;
 	}
-	
+
 	public static ArrayList<Character> separateWords(String w) {
 		ArrayList<Character> words = new ArrayList<>();
 		for (int i = 0; i<w.length(); i++) {
@@ -22,13 +22,14 @@ public class Anagrams {
 		}
 		return words;
 	}
-	
-	static boolean isAnagram(String a, String b) {
+
+	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
         String word1 = scan.next();
         String word2 = scan.next();
         scan.close();
-        boolean answer = false;	
+		String answer = "Not Anagrams";	
 		ArrayList<Character> w1 = separateWords(word1.toLowerCase());
 		w1.sort(null);
 		ArrayList<Character> w2 = separateWords(word2.toLowerCase());
@@ -36,13 +37,9 @@ public class Anagrams {
 		if (word1.length() == word2.length()) {
 			HashMap<Character, Integer> words1 = countWords(w1);
 			HashMap<Character, Integer> words2 = countWords(w2);
-			if (words1.equals(words2)) answer = true;
-		}		
-		System.out.println(answer);	
-    }
-
-	public static void main(String[] args) {		
-			
+			if (words1.equals(words2)) answer = "Anagrams";
+		}
+		System.out.println(answer);		
 	}
 
 }
